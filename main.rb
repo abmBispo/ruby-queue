dfdfcb59b3ad66fc3b59ee12bf55f91666bd05fa
Alan Borges <sr.alan.bispo@gmail.com>
2018-12-06 21:25:36 -0200

    Adicionado saída padrão stdout no arquivo output.json

--- main.rb
+++ main.rb
@@ -8,6 +8,7 @@ require_relative 'source/utils/hash'
 # Reading from STDIN
 stdin = ARGF.read
 parsed_input = JSON.parse(stdin)
+stdout = []
 
 # Instanciating queue
 queue = RubyQueue.new
@@ -27,10 +28,15 @@ parsed_input.each do |individual_command|
     when :job_request
       job = queue.dequeue(params[:agent_id])
       next if job.nil?
-      
-      puts "--------job_assigned-------"
-      puts "job_id: #{job.id}"
-      puts "agent_id: #{params[:agent_id]}"
+
+      stdout << {
+        job_assigned: {
+          job_id: job.id,
+          agent_id: params[:agent_id]
+        }
+      }
     end
   end
 end
+
+open('output.json', 'w') { |f| f.puts stdout.to_json }
